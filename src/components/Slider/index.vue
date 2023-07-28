77004a748c001a2ddd88e832552fef235be32db5
N777777 <84916981+N777777@users.noreply.github.com>
2023-07-28 17:26:34 +0800

    swiper_2

--- src/components/Slider/index.vue
+++ src/components/Slider/index.vue
@@ -1,23 +1,21 @@
 <template>
   <swiper
     :modules="modules"
-    :slides-per-view="1"
-    :space-between="50"
-    navigation
     :pagination="{ clickable: true }"
-    :scrollbar="{ draggable: false }"
+    :autoplay="{ delay: 3000 }"
     @swiper="onSwiper"
     @slideChange="onSlideChange"
   >
-    <swiper-slide><img class="bg-cover" src="@/assets/images/slider.png"></swiper-slide>
-    <swiper-slide><img class="bg-cover" src="@/assets/images/slider-test1.png"></swiper-slide>
-    <swiper-slide><img class="bg-cover" src="@/assets/images/slider-test2.png"></swiper-slide>
-    ...
+    <swiper-slide><img src="@/assets/images/slider.png"></swiper-slide>
+    <swiper-slide><img src="@/assets/images/slider-test1.png"></swiper-slide>
+    <swiper-slide><img src="@/assets/images/slider-test2.png"></swiper-slide>
+    
   </swiper>
 </template>
-<script>
+
+<script setup>
   // import Swiper core and required modules
-  import { Pagination, Zoom, Autoplay } from 'swiper/modules';
+  import { Pagination, Autoplay } from 'swiper/modules';
 
   // Import Swiper Vue.js components
   import { Swiper, SwiperSlide } from 'swiper/vue';
@@ -25,27 +23,7 @@
   // Import Swiper styles
   import 'swiper/css';
   import 'swiper/css/pagination';
-  import 'swiper/css/zoom';
   import 'swiper/css/autoplay';
 
-  // Import Swiper styles
-  export default {
-    components: {
-      Swiper,
-      SwiperSlide,
-    },
-    setup() {
-      const onSwiper = (swiper) => {
-        console.log(swiper);
-      };
-      const onSlideChange = () => {
-        console.log('slide change');
-      };
-      return {
-        onSwiper,
-        onSlideChange,
-        modules: [Pagination, Zoom, Autoplay ],
-      };
-    },
-  };
+  const modules = [Pagination, Autoplay];
 </script>
