702dad18e1244ae536faadf2842528b5db1db8d4
WilsonIsCoding <wilsoncheng0704.wc@gmail.com>
2023-10-01 13:56:22 +0800

    [修復]修改list 寫法

--- src/components/SortSelect/index.vue
+++ src/components/SortSelect/index.vue
@@ -1,16 +1,13 @@
 <template>
     <div class="flex justify-end mb-6">
         <select class="sortSelect border-2 border-gray-300 font-bold">
-            <option>依讚數排列</option>
-            <option>Dog</option>
-            <option>Cat</option>
-            <option>Hamster</option>
-            <option>Parrot</option>
-            <option>Spider</option>
-            <option>Goldfish</option>
+            <option v-for="(condition, i) in optionList" v-key="i">{{ condition }}</option>
         </select>
     </div>
 </template>
+<script setup>
+const optionList=['依讚數排列','Dog','Cat','Hamster','Parrot','Spider','Goldfish']
+</script>
 <style lang="scss">
 .sortSelect {
     margin-top: 10px ;
