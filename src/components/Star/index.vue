943ff5b0754183d893f6b1c1e5954d715e1b3eaf
WilsonIsCoding <wilsoncheng0704.wc@gmail.com>
2023-10-09 20:47:13 +0800

    [功能]阻止事件冒泡

--- src/components/Star/index.vue
+++ src/components/Star/index.vue
@@ -1,6 +1,6 @@
 <template>
-    <img v-if="props.starFullControl" class="star icon mr-4 cursor-pointer" src="@/assets/images/star-full.png" @click="$emit('toggleStar')"/>
-    <img v-else class="star icon mr-4 cursor-pointer" src="@/assets/images/star.png" @click="$emit('toggleStar')"/>
+    <img v-if="props.starFullControl" class="star icon mr-4 cursor-pointer" src="@/assets/images/star-full.png" @click.stop="$emit('toggleStar')"/>
+    <img v-else class="star icon mr-4 cursor-pointer" src="@/assets/images/star.png" @click.stop="$emit('toggleStar')"/>
 </template>
 <script setup>
 import { defineProps } from 'vue';
